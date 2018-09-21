ecc1c0bea2d76e55c79b6748d9f7423ae765c1c3
MansiGarg1810 <43470190+MansiGarg1810@users.noreply.github.com>
2018-09-21 16:25:05 +0530

    Create testps.ps1

---
+++ testps.ps1
@@ -0,0 +1,6 @@
+New-AzureRmDeployment `
+  -Name demoEmptyRG `
+  -Location southcentralus `
+  -TemplateUri https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/azure-resource-manager/emptyRG.json `
+  -rgName demogroup `
+  -rgLocation northcentralus
